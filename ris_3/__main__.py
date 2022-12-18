1bee49bda74ed5917fb1d2feada003167a466479
o.kyba <o.kyba@smartweb.com.ua>
2022-12-18 14:56:05 +0200

    RIS-3: add all queries

--- ris_3/__main__.py
+++ ris_3/__main__.py
@@ -21,27 +21,112 @@ goods = (
 sales = generate_sales(
     total_count=100,
     goods=goods,
-    max_goods_len=4,
+    max_goods_len=5,
     shops=('атб', 'фора', 'сільпо'),
     date_start=datetime(2022, 9, 17),
     date_end=datetime(2022, 12, 17),
 )
 
 with sql_sales_repository() as sql_repo, column_sales_repository() as column_repo:
-    log.info(sql_repo.count_all_sales())
-
     sql_repo.fill_db(sales)
     column_repo.fill_db(sales)
 
-    sql_count = sql_repo.count_all_sales()
-    column_count = column_repo.count_all_sales()
+    # 1. Порахувати кількість проданового товару
+    sql_count = sql_repo.count_sales()
+    column_count = column_repo.count_sales()
 
+    # 2. Порахувати вартість проданого товару
     sql_price = sql_repo.calculate_income()
     column_price = column_repo.calculate_income()
 
-    sql_goods_to_income = sql_repo.calculate_income_by_goods()
-    column_goods_to_income = column_repo.calculate_income_by_goods()
+    # 3. Порахувати вартість проданого товару за період
+    sql_price_date = sql_repo.calculate_income(
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_price_date = column_repo.calculate_income(
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+
+    # 4. Порахувати скільки було придбано товару А в мазазині В за період С
+    sql_count_by_shop_goods_date = sql_repo.count_sales(
+        goods='хліб',
+        shop='атб',
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_count_by_shop_goods_date = column_repo.count_sales(
+        goods='хліб',
+        shop='атб',
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+
+    # 5. Порахувати скільки було придбано товару А в усіх магазинах за період С
+    sql_count_by_goods_date = sql_repo.count_sales(
+        goods='хліб',
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_count_by_goods_date = column_repo.count_sales(
+        goods='хліб',
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+
+    # 6. Порахувати сумарну виручку магазинів за період С
+    sql_income_by_shops = sql_repo.calculate_income_by_shops(
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_income_by_shops = column_repo.calculate_income_by_shops(
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+
+    # 7. Вивести топ 10 купівель товарів по два за період С (наприклад масло, хліб - 1000 разів)
+    sql_top_2 = sql_repo.calculate_top(
+        items_in_bill=2,
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_top_2 = column_repo.calculate_top(
+        items_in_bill=2,
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+
+    # 8. Вивести топ 10 купівель товарів по три за період С (наприклад молоко, масло, хліб - 1000 разів)
+    sql_top_3 = sql_repo.calculate_top(
+        items_in_bill=3,
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_top_3 = column_repo.calculate_top(
+        items_in_bill=3,
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+
+    # 9. Вивести топ 10 купівель товарів по чотири за період С
+    sql_top_4 = sql_repo.calculate_top(
+        items_in_bill=4,
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
+    column_top_4 = column_repo.calculate_top(
+        items_in_bill=4,
+        date_start=datetime(2022, 10, 1),
+        date_end=datetime(2022, 11, 1),
+    )
 
     log.info(f'{sql_count=}, {column_count=}')
     log.info(f'{sql_price=}, {column_price=}')
-    log.info(f'{sql_goods_to_income=}, {column_goods_to_income=}')
+    log.info(f'{sql_price_date=}, {column_price_date=}')
+    log.info(f'{sql_count_by_shop_goods_date=}, {column_count_by_shop_goods_date=}')
+    log.info(f'{sql_count_by_goods_date=}, {column_count_by_goods_date=}')
+    log.info(f'{sql_income_by_shops=}, {column_income_by_shops=}')
+    log.info(f'{sql_top_2=}, {column_top_2=}')
+    log.info(f'{sql_top_3=}, {column_top_3=}')
+    log.info(f'{sql_top_4=}, {column_top_4=}')
--- ris_3/repositories/repo.py
+++ ris_3/repositories/repo.py
@@ -1,8 +1,12 @@
+import abc
+from datetime import datetime
 from contextlib import contextmanager
 from typing import Union, Type
 
-from sqlalchemy import select, func
+from sqlalchemy import select, func, desc
+from sqlalchemy.sql.functions import Function
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import aggregate_order_by
 
 from ris_3.bl import Sale
 from ris_3.repositories.sql.models import SqlSale
@@ -11,7 +15,7 @@ from ris_3.repositories.column.model import ColumnSale
 from ris_3.repositories.column.core import create_column_session
 
 
-class SalesRepository:
+class SalesRepository(abc.ABC):
     Model: Union[Type[SqlSale], Type[ColumnSale]]
 
     def __init__(self, session: Session):
@@ -38,16 +42,20 @@ class SalesRepository:
 
         self.session.commit()
 
-    def count_all_sales(self) -> int:
-        result = self.session.execute(
-            select(func.count()).select_from(self.Model)
-        )
-        return result.scalar_one()
+    def calculate_income(
+        self,
+        date_start: datetime = None,
+        date_end: datetime = None,
+    ) -> float:
+        query = select(func.sum(self.Model.price))
 
-    def calculate_income(self) -> float:
-        result = self.session.execute(
-            select(func.sum(self.Model.price))
-        )
+        if date_start:
+            query = query.where(self.Model.sale_date >= date_start)
+
+        if date_end:
+            query = query.where(self.Model.sale_date < date_end)
+
+        result = self.session.execute(query)
         return result.scalar_one()
 
     def calculate_income_by_goods(self) -> dict[str, float]:
@@ -58,13 +66,109 @@ class SalesRepository:
         result = self.session.execute(query)
         return {goods: income for goods, income in result.all()}
 
+    def count_sales(
+        self,
+        goods: str = None,
+        shop: str = None,
+        date_start: datetime = None,
+        date_end: datetime = None,
+    ) -> float:
+        query = select(func.count()).select_from(self.Model)
+
+        if goods:
+            query = query.where(self.Model.name == goods)
+
+        if shop:
+            query = query.where(self.Model.shop == shop)
+
+        if date_start:
+            query = query.where(self.Model.sale_date >= date_start)
+
+        if date_end:
+            query = query.where(self.Model.sale_date < date_end)
+
+        result = self.session.execute(query)
+        return result.scalar_one()
+
+    def calculate_income_by_shops(
+        self,
+        date_start: datetime = None,
+        date_end: datetime = None,
+    ) -> dict[str, float]:
+        query = (
+            select([self.Model.shop, func.sum(self.Model.price)])
+            .group_by(self.Model.shop)
+        )
+
+        if date_start:
+            query = query.where(self.Model.sale_date >= date_start)
+
+        if date_end:
+            query = query.where(self.Model.sale_date < date_end)
+
+        result = self.session.execute(query)
+        return {shop: income for shop, income in result.all()}
+
+    @abc.abstractmethod
+    def get_calculate_top_array_agg(self) -> Function:
+        raise NotImplemented
+
+    def calculate_top(
+        self,
+        items_in_bill: int,
+        top_count: int = 10,
+        date_start: datetime = None,
+        date_end: datetime = None,
+    ) -> list[tuple[list[str], int]]:
+        subquery = (
+            select(
+                self.get_calculate_top_array_agg()
+                .label('goods_array')
+            )
+            .group_by(self.Model.bill_id)
+            .having(func.count(self.Model.name) == items_in_bill)
+        )
+
+        if date_start:
+            subquery = subquery.where(self.Model.sale_date >= date_start)
+
+        if date_end:
+            subquery = subquery.where(self.Model.sale_date < date_end)
+
+        count = func.count(subquery.c.goods_array).label('count')
+        query = (
+            select(
+                subquery.c.goods_array,
+                count,
+            )
+            .select_from(subquery)
+            .group_by(subquery.c.goods_array)
+            .order_by(desc(count), subquery.c.goods_array)
+            .limit(top_count)
+        )
+        result = self.session.execute(query)
+        return result.all()
+
 
 class SqlSalesRepository(SalesRepository):
     Model = SqlSale
 
+    def get_calculate_top_array_agg(self) -> Function:
+        return func.array_agg(
+            aggregate_order_by(
+                self.Model.name,
+                self.Model.name,
+            )
+        )
+
 
 class ColumnSalesRepository(SalesRepository):
     Model = ColumnSale
+
+    def get_calculate_top_array_agg(self) -> Function:
+        return func.arraySort(
+            func.groupArray(self.Model.name)
+        )
 
 
 @contextmanager
